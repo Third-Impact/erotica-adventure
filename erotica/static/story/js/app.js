7e63465e9f056db71b49748444bda989a8e441cf
PhillipWitkin <pwitkin273@gmail.com>
2016-07-12 10:16:46 -0400

    refactored die simulation code so its totally awesome

--- erotica/static/story/js/app.js
+++ erotica/static/story/js/app.js
@@ -8,31 +8,90 @@ if (pickBranch) {
 		links = $('.branch-link')//document.getElementsByClassName('branch-link')
 		numBranches = links.length
 		rand = Math.floor(Math.random() * (numBranches))
-		scrollThroughLinks(300)
-
+		dieSim = dieSimulator(links)
+		scrollThroughLinks(dieSim)
 	  });
 }
 
-function scrollThroughLinks(time, linkNum){
+function scrollThroughLinks(die){
 	
-		numBranches = links.length
+		// numBranches = links.length
 		
-		rand = Math.floor(Math.random() * (numBranches))
+		// rand = Math.floor(Math.random() * (numBranches))
+		// while(linkNum == rand){
+		// 	rand = Math.floor(Math.random() * (numBranches))
+		// }
+		// selector = ".branch-link:eq("+rand+")"
+
+		// el = $(selector)
+
+		die.animation()
+
+		setTimeout(function(){
+			die.next()
+			if (die.time() >= 1000){
+				die.animationEnlarge()
+				return
+
+			} else {
+				scrollThroughLinks(die)
+			}
+
+		}, die.time())
+}
+
+
+
+var dieSimulator = function(links){
+
+	var time=400
+	var element = select()
+	var numBranches = links.length
+	var linkNum = rand = Math.floor(Math.random() * (numBranches))
+
+	function incTime(){
+		time*=1.15
+	}
+
+	function select(){
 		while(linkNum == rand){
 			rand = Math.floor(Math.random() * (numBranches))
 		}
-		selector = ".branch-link:eq("+rand+")"
-		$(selector).addClass('hover')
+		linkNum = rand
+		selector = ".branch-link:eq("+linkNum+")"
+		return $(selector)
+	}
 
+	return {
+		animation: function (){
+			element.animate({
+				letterSpacing:"2px"
+						// color: "black"
+			}, time/2.5,  function(){
+				element.animate({
+					letterSpacing: "-=2" 
+				}, time/2.5)
+			})
+		},
 
-		setTimeout(function(){
+		animationEnlarge: function(){
+			element.animate({
+				letterSpacing:"2px"
+			}, time, function(){
+				return element[0].click()
+			})
+		},
 
-			if (time >=1400){
-				return $(selector)[0].click()
-			} else {
-				$(selector).removeClass('hover')
-				time*=1.15
-				scrollThroughLinks(time, rand)
-			}
-		}, time)
+		time: function(){
+			return time
+		},
+
+		next: function(){
+		
+			element = select()
+
+			incTime()
+		}
+	}
+
 }
