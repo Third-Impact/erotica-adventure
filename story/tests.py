c958fbf312793b0afaabbdbe85c490a5446f2bc3
PhillipWitkin <pwitkin273@gmail.com>
2016-07-19 03:57:40 -0400

    finished test for endpoint

--- story/tests.py
+++ story/tests.py
@@ -28,17 +28,11 @@ class EndPointEligabilityTest(TestCase):
 	@classmethod
 	def setUpTestData(cls):
 		cls.u1 = create_user('TheDoc', 'enterthedalek', 'blah@example.come')
-		# cls.u1 = u1
 		cls.scene_origin = create_scene("origin", False, '', cls.u1)
 		print("origin id is " + str(cls.scene_origin.id))
-		cls.scene_t2 = create_scene("Something happens", False, '', cls.u1) 
+		cls.scene_t2 = create_scene("Something happens", False, '', cls.u1)
+		cls.scene_t3 = create_scene("Candidate endpoint", False, '', cls.u1) 
 
-	# def test_index_status(self):
-	# 	response = self.client.get('/erotica/')
-	# 	self.assertEqual(response.status_code, 200)
-
-	# def test_single_new_scene_cannot_be_open(self):
-	# 	self.assertEqual(self.scene_t2.can_be_open(), False)
 
 	def test_single_new_scene_cannot_be_end(self):
 		self.assertEqual(self.scene_t2.can_be_end(), False)
@@ -49,10 +43,33 @@ class EndPointEligabilityTest(TestCase):
 
 	def test_adding_a_branch_from_scene_to_origin_can_be_end(self):
 		create_branch(self.scene_origin, self.scene_t2, "a branch")
-		self.scene_t3 = create_scene("Candidate endpoint", False, '', self.u1)
-		self.assertEqual(self.scene_t2.can_be_end(), True)
 		create_branch(self.scene_t2, self.scene_t3, "branch to end")
 		self.assertEqual(self.scene_t2.can_be_end(), False)
 		self.assertEqual(self.scene_t3.can_be_end(), True)
 		create_branch(self.scene_t3, self.scene_origin, "branch to origin")
 		self.assertEqual(self.scene_t3.can_be_end(), True)
+
+	def test_adding_a_branch_from_scene_to_save_point_can_be_end(self):
+		create_branch(self.scene_origin, self.scene_t2, "a branch")
+		create_branch(self.scene_t2, self.scene_t3, "branch to end")
+		self.assertEqual(self.scene_t3.can_be_end(), True)
+		self.scene_t4 = create_scene("will be save point", False, '', self.u1)
+		create_branch(self.scene_t3, self.scene_t4, "branch to save point")
+		self.assertEqual(self.scene_t3.can_be_end(), False)
+		self.scene_t4.save_point = True
+		self.scene_t4.save()
+		self.assertEqual(self.scene_t3.can_be_end(), True)
+
+	def test_adding_a_branch_from_scene_to_other_will_not_be_end(self):
+		create_branch(self.scene_origin, self.scene_t2, "a branch to end")
+		self.assertEqual(self.scene_t2.can_be_end(), True)
+		create_branch(self.scene_t2, self.scene_t3, "branch from end")
+		self.assertEqual(self.scene_t2.can_be_end(), False)
+
+
+	# def test_index_status(self):
+	# 	response = self.client.get('/erotica/')
+	# 	self.assertEqual(response.status_code, 200)
+
+	# def test_single_new_scene_cannot_be_open(self):
+	# 	self.assertEqual(self.scene_t2.can_be_open(), False)
